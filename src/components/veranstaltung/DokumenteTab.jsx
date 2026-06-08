5ca625170a81f2bfc133774b2d5a24f9ea4d54bf
5vcybbv2hx-debug <5vcybbv2hx@privaterelay.appleid.com>
2026-06-08 15:59:20 +0200

    design: DokumenteTab.jsx – Spinner Narrenzunft

--- src/components/veranstaltung/DokumenteTab.jsx
+++ src/components/veranstaltung/DokumenteTab.jsx
@@ -177,7 +177,7 @@ export default function DokumenteTab({ veranstaltung, isAdmin, veranstaltungsNam
 
   if (loading) return (
     <div className="flex items-center justify-center py-12">
-      <div className="w-6 h-6 border-4 border-border border-t-primary rounded-full animate-spin" />
+      <div className="w-6 h-6 border-[3px] border-border border-t-primary rounded-full animate-spin" />
     </div>
   );
 
