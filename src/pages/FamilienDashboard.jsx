d056ee47a1d1277e4bb05b97b27c28e3c50771a2
5vcybbv2hx-debug <5vcybbv2hx@privaterelay.appleid.com>
2026-06-08 15:58:17 +0200

    design: FamilienDashboard.jsx – Spinner-Konsistenz Narrenzunft

--- src/pages/FamilienDashboard.jsx
+++ src/pages/FamilienDashboard.jsx
@@ -55,7 +55,7 @@ export default function FamilienDashboard() {
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-[60vh]">
-        <div className="w-8 h-8 border-4 border-border border-t-primary rounded-full animate-spin" />
+        <div className="w-8 h-8 border-[3px] border-border border-t-primary rounded-full animate-spin" />
       </div>
     );
   }
