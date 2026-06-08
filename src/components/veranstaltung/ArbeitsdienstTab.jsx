3abe30f6ebbdd64c6da6f9a170de87461a534c02
5vcybbv2hx-debug <5vcybbv2hx@privaterelay.appleid.com>
2026-06-08 15:59:19 +0200

    design: ArbeitsdienstTab.jsx – Spinner Narrenzunft

--- src/components/veranstaltung/ArbeitsdienstTab.jsx
+++ src/components/veranstaltung/ArbeitsdienstTab.jsx
@@ -82,7 +82,7 @@ export default function ArbeitsdienstTab({ veranstaltung, isAdmin }) {
 
   if (loading) return (
     <div className="flex items-center justify-center py-12">
-      <div className="w-6 h-6 border-4 border-border border-t-primary rounded-full animate-spin" />
+      <div className="w-6 h-6 border-[3px] border-border border-t-primary rounded-full animate-spin" />
     </div>
   );
 
